fb727acae6cb355379f8a59b60fda54c5172b210
ibrahimnassar31 <85653398+ibrahimnassar31@users.noreply.github.com>
2025-04-22 12:53:00 +0300

    loading logic

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,28 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppSelector } from '../src/redux/hooks';
 
 export default function Home() {
   const router = useRouter();
   const { user } = useAppSelector((state) => state.auth);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!user) {
       router.push('/signup');
+    } else {
+      setIsLoading(false);
     }
-  }, [user]);
+  }, [user , router]);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center w-screen h-screen bg-gray-100">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-gray-100">
